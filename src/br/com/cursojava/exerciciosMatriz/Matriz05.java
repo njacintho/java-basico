3c461bffe8ab6e5090926696f896b6b637180766
9001129 <9001129@ELOM02TZ4W-N.elo.corp>
2022-01-19 14:40:18 -0300

    exec matriz

--- src/br/com/cursojava/exerciciosMatriz/Matriz05.java
+++ src/br/com/cursojava/exerciciosMatriz/Matriz05.java
@@ -2,13 +2,13 @@ package br.com.cursojava.exerciciosMatriz;
 
 import java.util.Scanner;
 
-public class Matriz04 {
+public class Matriz05 {
 
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
 
-		String[][] agenda = new String[31][24]; // 31 dias e 24 horas.
+		String[][][] agenda = new String[31][12][8]; // 31 dias e 24 horas.
 
 		boolean sair = false;
 		int opcao;
@@ -33,12 +33,24 @@ public class Matriz04 {
 						System.out.println("Dia invalido digite novamente!");
 					}
 				}
+				boolean mesValido = false;
+				int mes = 0;
+
+				while (!mesValido) {
+					System.out.println("Entre com o mês: ");
+					mes = scan.nextInt();
+					if (mes > 0 && mes <= 12) {
+						mesValido = true;
+					} else {
+						System.out.println("Mes invalido digite novamente!");
+					}
+				}
 				boolean horaValida = false;
 				int hora = 0;
 				while (!horaValida) {
-					System.out.println("Entre com o horario: ");
+					System.out.println("Entre com o horario até 8: ");
 					hora = scan.nextInt();
-					if (hora >= 0 && hora <= 24) {
+					if (hora >= 0 && hora <= 8) {
 						horaValida = true;
 					} else {
 						System.out.println("Hora invalida digite novamente!");
@@ -46,11 +58,11 @@ public class Matriz04 {
 				}
 				if (opcao == 1) {
 					System.out.println("Digite o compromisso");
-					agenda[--dia][hora] = scan.next();
+					agenda[--dia][--mes][--hora] = scan.next();
 				} else if (opcao == 2) {
 					dia--;
 					System.out.println("O compromisso agendado é: ");
-					System.out.println(agenda[dia][hora]);
+					System.out.println(agenda[dia][--mes][--hora]);
 				}
 			} else if (opcao == 0) {
 				sair = true;
---
+++ src/br/com/cursojava/exerciciosMatriz/Matriz06.java
@@ -0,0 +1,16 @@
+package br.com.cursojava.exerciciosMatriz;
+
+import java.util.Scanner;
+// correção aula 20 parte 4
+public class Matriz06 {
+
+	public static void main(String[] args) {
+
+		Scanner sca = new Scanner(System.in);
+		
+		char[][] jogoDaVelha = new char[3][3];
+		
+		
+	}
+
+}
