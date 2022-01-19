1649a1ac148d1f7d56e06cf0e8a339db16e89be4
njacintho <njacintho@ELOM02TZ4W-N.elo.corp>
2022-01-19 14:59:46 -0300

    teste

--- src/br/com/cursojava/aula21/ForEach.java
+++ src/br/com/cursojava/aula21/ForEach.java
@@ -7,13 +7,12 @@ public class ForEach {
 	public static void main(String[] args) {
 		int[] notas = new int[10];
 		Random random = new Random();
-		
 		for(int i = 0; i < notas.length; i++) {
 			notas[i] = random.nextInt(10);
 		}
 		
 		System.out.println("Usando o forEach");
-		
+	
 		for (int nota : notas) {
 			System.out.println(nota);
 		}
