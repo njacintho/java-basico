0ae1862ba9cb3a3ac13ffdf1f089c6ef9318f267
9001129 <9001129@ELOM02TZ4W-N.elo.corp>
2022-02-02 19:53:53 -0300

    exec metodos aluno

---
+++ src/br/com/cursojava/exerciciosMetodos/Aluno.java
@@ -0,0 +1,40 @@
+package br.com.cursojava.exerciciosMetodos;
+
+public class Aluno {
+	String nome;
+	int matricula;
+	String curso;
+	String[] disciplinas = new String[3];
+	double[][] notas = new double[3][4];
+
+
+	void motraDados() {
+		System.out.println("Aluno: " + nome + " - matricula: " + matricula );
+		System.out.println("Nome do curso: " + curso);
+
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println("Notas da disciplina de " + disciplinas[i]);
+			for (int j = 0; j < notas[i].length; j++) {
+				System.out.print(notas[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+	}
+	double obeterMedia(int indice) {
+		double soma = 0;
+		for (int i = 0; i < notas.length; i++) {
+			soma += notas[indice][i];
+		}
+		double media = soma / 4;
+		return media;
+	}
+
+	boolean verificarAprovacao(int indice) {
+		if (obeterMedia(indice) >= 7) {
+			return true;
+		}
+		return false;
+	}
+}
---
+++ src/br/com/cursojava/exerciciosMetodos/TesteAluno.java
@@ -0,0 +1,52 @@
+package br.com.cursojava.exerciciosMetodos;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class TesteAluno {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		Aluno aluno = new Aluno();
+
+		System.out.println("Entre com nome do aluno");
+		aluno.nome = scan.next();
+
+		System.out.println("Entre com a matricula: ");
+		aluno.matricula = scan.nextInt();
+
+		System.out.println("Entre com nome o curso");
+		aluno.curso = scan.next();
+
+		for (int i = 0; i < aluno.disciplinas.length; i++) {
+			System.out.println("Entre com as disciplinas do curso");
+			aluno.disciplinas[i] = scan.next();
+		}
+		
+
+		for (int i = 0; i < aluno.notas.length; i++) {
+			System.out.println("Notas da disciplina de " + aluno.disciplinas[i]);
+			for (int j = 0; j < aluno.notas[i].length; j++) {
+				System.out.println("Entre com as notas");
+				aluno.notas[i][j] = scan.nextDouble();
+			}
+		}
+		System.out.println("------------------------------------------------------------------------");
+		
+		aluno.motraDados();
+		
+		System.out.println("------------------------------------------------------------------------");
+		
+		for(int i = 0; i <aluno.disciplinas.length; i++) {
+			
+			if(aluno.verificarAprovacao(i)) {
+				System.out.println("Disciplina: " + aluno.disciplinas[i] + ". Está aprovado");
+			}else {
+				System.out.println("Disciplina: " + aluno.disciplinas[i] + ". Está reprovado");
+			}
+		}
+
+	}
+
+}
